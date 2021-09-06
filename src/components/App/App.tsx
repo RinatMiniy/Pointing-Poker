0be4c6c8d5eccce71a57d71caa5e10a0dcad9b35
RinatMiniy <RinatMiniy@gmail.com>
2021-09-06 13:24:59 +0300

    feat: add header and footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Header } from "../Header/Header";
+import { Footer } from "../Footer/Footer";
 import "./app.scss";
 
 export const App: React.FC = () => {
-  return <div>1</div>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 };
