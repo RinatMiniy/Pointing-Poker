286e44f15cd6993c28198a1351f2a6efe21e7cba
RinatMiniy <RinatMiniy@gmail.com>
2021-09-06 13:24:59 +0300

    feat: add header and footer

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./header.scss";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="blue__line"></div>
+      <div className="aquamarine__line"></div>
+      <img className="logo" src="./logo.svg" alt="logo" />
+    </header>
+  );
+};
