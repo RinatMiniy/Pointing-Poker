7c23a8e483d26ab08dbbc79b70e5050ef25c25d4
RinatMiniy <RinatMiniy@gmail.com>
2021-09-16 12:01:48 +0300

    fix: some styles

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,6 +1,7 @@
 import { Button } from "../../sharedComponents/button/button";
 import { InputText } from "../../sharedComponents/inputText/InputText";
 import styles from "./mainPage.module.scss";
+import cn from "classnames";
 
 export const MainPage = () => {
   return (
@@ -11,18 +12,18 @@ export const MainPage = () => {
         className={styles.mainLogo}
       />
       <h2 className={styles.mainTitle}>Start your planning:</h2>
-      {/* <label className = {styles.mainLabel}> */}
-      <p>Create session:</p>
-      <Button text="Start new game" isPrimary={true} isLong={true} />
-      {/* </label> */}
-      <h2 className={styles.mainTitle}>OR:</h2>
-      {/* <label className = {styles.mainLabel}> */}
-      <p> Connect to lobby by URL:</p>
-      <div className={styles.connectLobby}>
-        <InputText field="" name="linkLobby" />
-        <Button text="Connect" isPrimary={true} isLong={true} />
+      <div className={cn(styles.mainLabel, styles.fixlabel)}>
+        <p>Create session:</p>
+        <Button text="Start new game" isPrimary={true} isLong={true} />
       </div>
-      {/* </label> */}
+      <h2 className={styles.mainTitle}>OR:</h2>
+      <div className={styles.mainLabel}>
+        <p> Connect to lobby by URL:</p>
+        <div className={styles.connectLobby}>
+          <InputText field="" name="linkLobby" />
+          <Button text="Connect" isPrimary={true} isLong={true} />
+        </div>
+      </div>
     </div>
   );
 };
