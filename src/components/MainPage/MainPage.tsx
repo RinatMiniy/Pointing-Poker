c155e15bd9af4eb7b1371a8d2abded052b200e16
Marina <gma18@list.ru>
2021-09-20 01:58:07 +0400

    feat: add opening registration form

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,9 +1,13 @@
+import React from "react";
 import { Button } from "../../sharedComponents/button/button";
 import { InputText } from "../../sharedComponents/inputText/InputText";
+import { RegisterForm } from "../registerForm/RegisterForm";
 import styles from "./mainPage.module.scss";
 import cn from "classnames";
 
-export const MainPage = () => {
+export const MainPage: React.FC = () => {
+  const [openRegistration, setOpenRegistration] = React.useState(false);
+
   return (
     <div className={styles.mainPage}>
       <img
@@ -14,7 +18,12 @@ export const MainPage = () => {
       <h2 className={styles.mainTitle}>Start your planning:</h2>
       <div className={cn(styles.mainLabel, styles.fixlabel)}>
         <p>Create session:</p>
-        <Button text="Start new game" isPrimary={true} isLong={true} />
+        <Button
+          text="Start new game"
+          isPrimary={true}
+          isLong={true}
+          onClick={() => setOpenRegistration(true)}
+        />
       </div>
       <h2 className={styles.mainTitle}>OR:</h2>
       <div className={styles.mainLabel}>
@@ -24,6 +33,7 @@ export const MainPage = () => {
           <Button text="Connect" isPrimary={true} isLong={true} />
         </div>
       </div>
+      {openRegistration && <RegisterForm />}
     </div>
   );
 };
